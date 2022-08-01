d540401b1b1f0c33ad926c9390dd106f0ac40502
artyZinchenko <zinchenko.artem96@gmail.com>
2022-08-01 12:57:59 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
   {
@@ -72,7 +74,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
