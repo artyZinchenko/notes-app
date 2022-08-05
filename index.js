15dbd9934d8c3a3b0408f7188ca92c6a3195292e
artyZinchenko <zinchenko.artem96@gmail.com>
2022-08-05 17:12:54 +0300

    fix

--- index.js
+++ index.js
@@ -8,20 +8,6 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  },
-})
-
 // let notes = [
 //   {
 //     id: 1,
